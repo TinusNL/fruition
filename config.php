e1cf40f1b0c3b541b964fe4808814a3e74954bd0
Tinus_NL <66055492+TinusNL@users.noreply.github.com>
2023-10-03 10:25:42 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
-define('URL_PREFIX', 'fruition'); // Used in modules/Router.php
+// Router
+define('URL_PREFIX', 'fruition');
 
 // Database Connection
 define('DATABASE_HOST', 'localhost');
@@ -8,6 +9,7 @@ define('DATABASE_USER', 'root');
 define('DATABASE_PASS', '');
 define('DATABASE_NAME', 'fruition');
 
+// Autoload modules when used
 spl_autoload_register(function ($class_name) {
     if (file_exists('modules/' . $class_name . '.php')) {
         require_once 'modules/' . $class_name . '.php';
