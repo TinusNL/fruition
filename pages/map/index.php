61dddc63d9d44749e63b84e77e4fb6851b45ce65
Tinus_NL <66055492+TinusNL@users.noreply.github.com>
2023-10-03 14:32:22 +0100

    Map Changes

--- pages/map/index.php
+++ pages/map/index.php
@@ -1,7 +1,15 @@
 <body class="map">
+    <!-- Leaflet -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
+    <!-- Easybutton -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.css">
+    <script src="https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.js"></script>
+
     <div id="leaflet-map"></div>
+    <script>
+        var markerJson = '[{"id":1,"type":"apple","lat":50.373380,"lng":-4.142650},{"id":2,"type":"apple","lat":50.370380,"lng":-4.142650}]';
+    </script>
     <script src="./scripts/leaflet.js"></script>
 </body>
--- scripts/leaflet.js
+++ scripts/leaflet.js
@@ -1,10 +1,29 @@
-var map = L.map('leaflet-map').setView([51.505, -0.09], 13)
+var map = L.map('leaflet-map').setView([50.370380, -4.142650], 15)
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
+var locationMarker
 
-var marker = L.marker([51.5, -0.09]).addTo(map)
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.")
+var btn = L.easyButton('<img src="./assets/location-crosshairs-solid.svg" id="location-crosshair">', function () {
+    navigator.geolocation.getCurrentPosition((position) => {
+        map.setView([position.coords.latitude, position.coords.longitude], 19, { pan: { animate: true } })
+
+        if (locationMarker) {
+            locationMarker.removeFrom(map)
+        }
+
+        locationMarker = L.circle([position.coords.latitude, position.coords.longitude], {
+            color: '#006ACD',
+            fillColor: '#006ACD',
+            radius: 10
+        }).addTo(map)
+    })
+}).addTo(map)
+
+var markers = JSON.parse(markerJson)
+markers.forEach(markerInfo => {
+    var marker = L.marker([markerInfo.lat, markerInfo.lng]).addTo(map)
+    marker.bindPopup(`<b>${markerInfo.type}</b></br>${markerInfo.id}`)
+})
