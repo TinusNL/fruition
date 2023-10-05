6c4e3bd28892a11989c5242105e1e7a467bde158
Chloe7725 <94675582+Chloe7725@users.noreply.github.com>
2023-10-05 14:15:10 +0100

    Version 1 front-end

---
+++ pages/signup/index.php
@@ -0,0 +1,24 @@
+<div class="sign-up">
+    <div class="form">
+        <h1>Sign Up</h1>
+        <form action="" method="post" id="form">
+            <label for="email">Email</label>
+            <input type="text">
+            <label for="number">Number</label>
+            <input type="text">
+            <label for="fullname">Full name</label>
+            <input type="text">
+            <label for="password">Password</label>
+            <input type="text">
+            <label for="confirm_password">Confirm password</label>
+            <input type="text">
+            <div id="button">
+                <label for="text">Dont have an account? <a href="#">Sign in</a></label>
+                <button>Sign in</button>
+            </div>
+        </form>
+    </div>
+    <div class="side">
+
+    </div>
+</div>
