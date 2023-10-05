3abfd65809af2be5ba13a2757f5109ac1b1c0fe4
islam-gsm <i.guessoum2005@gmail.com>
2023-10-05 11:50:43 +0100

    created a login page

---
+++ pages/login/index.php
@@ -0,0 +1,24 @@
+
+<body>
+<div class="login-container">
+        <div class="login-form">
+            <div class="login-title">Login</div>
+            <form method="post" action="login.php">
+                <div class="input-container">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="email" placeholder="Your email" required>
+                </div>
+                <div class="input-container">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Your password" required>
+                </div>
+                <button type="submit" class="login-button">Log In</button>
+            </form>
+            <div class="signup-link">
+                Don't have an account? <a href="signup.php">Sign Up</a>
+            </div>
+        </div>
+    </div>
+    <div class="logosvg"> 
+    <img src="./assets/logo.svg" alt="logo">
+</div>
