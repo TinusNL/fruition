aebdbc93432ce140a99d7ed97a09dcd26f539e76
AsaadHajar <6019679@mborijnland.nl>
2023-10-05 09:58:23 +0100

    +submissions

--- pages/submissions/index.php
+++ pages/submissions/index.php
@@ -1,66 +1,12 @@
+<?php
 
+?>
 <div>
-    <?php
-    use PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\src\Exception;
-    require 'PHPMailer/PHPMailer/src/Exception.php';
-    require 'PHPMailer/PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/PHPMailer/src/SMTP.php';
-    
-    // Process the form when it is submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Collect data from the form
-        $location = $_POST["location"];
-        $name = $_POST["name"];
-        $fruitType = $_POST["fruitType"];
-        $description = $_POST["description"];
-        $email = $_POST["email"];
+    <form action="send.php" method="post">
+        Email <input type="email"  name="email"> <br>
+        Subject <input type="text"  name="subject"> <br>
+        Messege <input type="text"  name="message"> <br>
 
-        // Photo upload processing
-        if (isset($_FILES["photo"])) {
-            $uploadDir = "uploads/";
-            $uploadFile = $uploadDir . basename($_FILES["photo"]["name"]);
-        }
-
-        // Here you can add the logic for sending the confirmation email
-
-        // Example: Send a simple confirmation email
-        $to = $_POST["email"];
-        $subject = "Form Confirmation";
-        $message = "Location: $location\n";
-        $message .= "Name: $name\n";
-        $message .= "Fruit Type: $fruitType\n";
-        $message .= "Description: $description\n";
-        $headers = "From: asaadhajar6@gamail.com";
-
-        mail($to, $subject, $message, $headers);
-    }
-    ?>
-
-    <h2>Form</h2>
-    <form method="post" enctype="multipart/form-data">
-        <label for="location">Location:</label>
-        <input type="text" name="location" required><br>
-
-        <label for="name">Name:</label>
-        <input type="text" name="name" required><br>
-
-        <label for="fruitType">Fruit Type:</label>
-        <input type="text" name="fruitType" required><br>
-
-        <label for="email">Email:</label>
-        <input type="email" name="email"><br>
-
-        <input id="emailConfirmation" type="checkbox"><span class="checkmark"></span>
-        <label for="emailConfirmation">Send me an mail</label><br>
-        
-        <label for="photo">Upload Photo:</label>
-        <input type="file" name="photo" accept="image/*"><br>
-
-        <label for="description">Description:</label><br>
-        <textarea name="description" rows="4" cols="50" required></textarea><br>
-
-        <input type="submit" value="Submit">
+        <button type="submit" id="send" name="send">Send</button>
     </form>
 </div>
-
---
+++ pages/submissions/send.php
@@ -0,0 +1,40 @@
+<?php
+use  PHPMailer\PHPMailer\PHPMailer;
+use  PHPMAiler\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if (isset($_POST['send'])) {
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'fruitioneuropa@gmail.com';
+    $mail->Password = 'kckqnopsmgiehsux';
+    $mail->SMTPSecrue = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('fruitioneuropa@gmail.com');
+
+    $mail->addAddress($_POST['email']);
+
+    $mail->isHTML(true);
+
+    $mail->Subject = $_POST['subject'];
+    $mail->Body = $_POST['message'];
+    
+    $mail->send();
+
+
+    echo 
+    "
+    <script>
+    alert('sent successfully');
+    document.location.href = 'index.php';
+    </script>
+    ";
+}
+?>
