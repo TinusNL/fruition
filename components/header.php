7947519dfef5c0e5232e0a069311f666f1fafbd7
Tinus_NL <66055492+TinusNL@users.noreply.github.com>
2023-10-05 10:05:19 +0100

    Bottom Bar

--- components/header.php
+++ components/header.php
@@ -1,7 +1,18 @@
 <nav class="header">
-    <div class="top"></div>
+    <div class="top">
+        <div class="search">
+            <input type="text" placeholder="Search">
+        </div>
+        <div class="categories">
+
+        </div>
+    </div>
     <div class="bottom">
-        <div class="actions"></div>
+        <div class="actions">
+            <a href="#"><img src="./assets/icons/user.svg" alt="Profile"></a>
+            <a href="#"><img src="./assets/icons/settings.svg" alt="Settings"></a>
+            <a href="#"><img src="./assets/icons/submission.svg" alt="Submission"></a>
+        </div>
         <div class="logo">
             <img src="./assets/logo.svg" alt="Fruition Logo">
         </div>
