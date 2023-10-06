64ee5d54eb2e04bc69dd307b89d360924f8e8b91
paraaa <94382784+ParaaBK@users.noreply.github.com>
2023-10-06 12:53:01 +0200

    Main and location details merged

--- scripts/leaflet.js
+++ scripts/leaflet.js
@@ -1,70 +1,67 @@
 // Background
 const tileLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-})
+});
 
-// Seting up marker layers
-// const markers = JSON.parse(markerJson)
-const markerLayers = {}
+// Set up marker layers
+const markerLayers = {};
+
+const markers = JSON.parse('[{ "id": 1, "photoName": "77069d90-e7e1-47a9-b5e0-41d70e45a956.webp", "type": "apple", "location": { "lat": 50.373380, "lng": -4.142650 }, "timestamp": 1633440000, "createdBy": "John Doe", "userId": 123, "name": "Apple Tree 1", "typeId": 1, "seasonId": 2 }, { "id": 2, "type": "apple", "location": { "lat": 50.370380, "lng": -4.142650 }, "timestamp": 1633441200, "createdBy": "Jane Smith", "photoName": "andere-foto-naam.webp", "userId": 456, "name": "Apple Tree 2", "typeId": 1, "seasonId": 3 }]');
 
 markers.forEach(markerInfo => {
-    const marker = L.marker([markerInfo.lat, markerInfo.lng], { icon: leafletIcons[markerInfo.type], riseOnHover: true })
-    marker.bindPopup(`<b>${markerInfo.type}</b></br>${markerInfo.id}`)
+    const marker = L.marker([markerInfo.location.lat, markerInfo.location.lng], {
+        icon: L.divIcon({ className: 'custom-icon', html: markerInfo.type }),
+        riseOnHover: true
+    });
 
-    markerLayers[markerInfo.type] = markerLayers[markerInfo.type] || L.layerGroup()
-    marker.addTo(markerLayers[markerInfo.type])
-})
+    marker.bindPopup(`
+        <div class="popup-container">
+            <img src="Images/${markerInfo.photoName}" alt="Marker Photo" class="popup-img">
+            <p class="type">Type: ${markerInfo.type}</p>
+            <p class="createdby">Created by: ${markerInfo.createdBy}</p>
+            <p class="timestamp">Timestamp: ${new Date(markerInfo.timestamp).toLocaleString()}</p>
+            <p class="season">Season: ${markerInfo.season}</p>
+            <p class="location"><a href="https://www.google.com/maps?q=${markerInfo.location.lat},${markerInfo.location.lng}" target="_blank">Location: ${markerInfo.location.lat}, ${markerInfo.location.lng}</a></p>
+        </div>
+    `);
+
+    markerLayers[markerInfo.type] = markerLayers[markerInfo.type] || L.layerGroup();
+    marker.addTo(markerLayers[markerInfo.type]);
+});
 
 // Create map
 const map = L.map('leaflet-map', {
     center: [50.370380, -4.142650],
     zoom: 15,
     layers: [tileLayer, ...Object.values(markerLayers).flat()]
-})
+});
 
 // Current Location
-var locationMarker
+let locationMarker;
 const btn = L.easyButton('<img src="./assets/icons/location-crosshair.svg" id="location-crosshair">', function () {
     navigator.geolocation.getCurrentPosition((position) => {
-        map.setView([position.coords.latitude, position.coords.longitude], 16, { pan: { animate: true } })
+        map.setView([position.coords.latitude, position.coords.longitude], 16, { pan: { animate: true } });
 
         if (locationMarker) {
-            locationMarker.removeFrom(map)
+            locationMarker.removeFrom(map);
         }
 
         locationMarker = L.circle([position.coords.latitude, position.coords.longitude], {
             color: '#006ACD',
             fillColor: '#006ACD',
             radius: 10
-        }).addTo(map)
-    })
-}).addTo(map)
-
-var markers = JSON.parse(markerJson);
-markers.forEach(markerInfo => {
-    var marker = L.marker([markerInfo.location.lat, markerInfo.location.lng]).addTo(map);
-
-    var timestamp = new Date(markerInfo.timestamp);
-    var photoUrl = `Images/${markerInfo.photoName}`;
-
-    // Convert latitude and longitude to a Google Maps link
-    var googleMapsLink = `https://www.google.com/maps?q=${markerInfo.location.lat},${markerInfo.location.lng}`;
-
-    marker.bindPopup(`
-        <div class="popup-container">
-            <p class="timestamp">${timestamp.toLocaleString()}</p>
-            <img src="${photoUrl}" alt="Marker Photo" class="popup-img">
-            <p class="type">Type: ${markerInfo.type}</p>
-            <p class="createdby">Created by: ${markerInfo.createdBy}</p>
-            <p class="season">Season: ${markerInfo.season}</p>
-            <p class="location"><a href="${googleMapsLink}" target="_blank">Location: ${markerInfo.location.lat}, ${markerInfo.location.lng}</a></p>
-        </div>
-    `, {
-        className: 'custom-popup'
+        }).addTo(map);
     });
-});
+}).addTo(map);
 
+// Open Functions
+function hideMarkerType(type) {
+    map.removeLayer(markerLayers[type]);
+}
 
+function showMarkerType(type) {
+    map.addLayer(markerLayers[type]);
+}
 
 
 
@@ -90,10 +87,3 @@ markers.forEach(markerInfo => {
 
 // marker.bindPopup(popupContent);
 // Open Functions
-function hideMarkerType(type) {
-    map.removeLayer(markerLayers[type])
-}
-
-function showMarkerType(type) {
-    map.addLayer(markerLayers[type])
-}
