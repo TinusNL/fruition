a2b455e07477b739ba31ea5a620c8421b3a0c5e2
Misha Opstal <6018369@mborijnland.nl>
2023-10-06 16:12:23 +0100

    testing

--- pages/login/index.php
+++ pages/login/index.php
@@ -1,5 +1,5 @@
-<div class="main_login">
-    <div class="left_login">
+<div class="main_authenticate">
+    <div class="left_authenticate">
         <h1 class="login-text">Login</h1>
         <div class="login_error">
             <?php
@@ -8,33 +8,34 @@
             }
             ?>
         </div>
-        <div class="form-login">
+        <div class="form-authenticate">
             <form action="authenticate" method="post" class="login-form"><br>
                 <label for="email">Email</label><br>
                 <input class="input-box" type="email" name="email" placeholder="    janedoe@mail.com">
                 <br>
                 <label for="password">Password</label><br>
                 <input class="input-box" type="password" name="password" placeholder="  *******">
-                <div class="account-signin">
-                    <p>Don't have an account? <a href="register" class="sign-up">Sign up</a></p>
-                    <input type="submit" name="login" class="sign-in" value="Sign in">
+                <input type="submit" name="login" class="authenicate_l" value="Sign in">
+                <div class="account-authenticate">
+                    <p>Don't have an account? <a href="signup" class="authenticate">Sign up</a></p>
                 </div>
                 <div class="login_extra">
                     <a href="forgot">Forgot Password</a>
                 </div>
             </form>
             <div class="socials">
-                <img src="./assets/Discord.svg" alt="">
-                <img src="./assets/Facebook.svg" alt="">
-                <img src="./assets/Instagram.svg" alt="">
-                <img src="./assets/Pinterest.svg" alt="">
+                <img src="assets/Google.svg" alt="Google" class="img_g" >
+                <img src="assets/Discord.svg" alt="Discord" class="img_g" >
+                <img src="assets/Facebook.svg" alt="Facebook" class="img_g" >
+                <img src="assets/Instagram.svg" alt="Instagram" class="img_g" >
+                <img src="assets/Pinterest.svg" alt="Pinterest" class="img_g" >
             </div>
-            <div class="logo-hidden-login">
+            <div class="logo-hidden-authenticate">
             <img src="./assets/logo_light.png" alt="logo" class="logo-mobile">
             </div>
         </div>
     </div>
-    <div class="right_login">
+    <div class="right_authenticate">
         <img src="./assets/logo.svg" alt="logo" class="logo">
         <h2>Free Food For All</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie luctus tellus, at viverra purus pulvinar vitae. Vivamus vitae lobortis magna, mollis mollis orci.</p>
