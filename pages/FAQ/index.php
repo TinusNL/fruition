d52c82038bf7646fef044048bd3289a2c5b31be5
nathan <146750678+nathanHood69@users.noreply.github.com>
2023-10-10 11:20:32 +0100

    basic html

---
+++ pages/FAQ/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>FAQ Page</title>
+</head>
+<body>
+    <h1>FAQ</h1>
+    <h3>frequently asked questions:</h3>
+    <h5>How do I secure my account?</h5>
+    <h5>how to add a new location?</h5>
+    <h5>how to upload images</h5>
+    <h5>how to change email</h5>
+    <h3>Need further assistance? feel free to chat with ourchatbot Fuity!</h3>
+</body>
+</html>
