ad7e777acf8b889f4daecc1e16dae8875a517ab1
Tinus_NL <66055492+TinusNL@users.noreply.github.com>
2023-10-10 15:23:11 +0100

    Update faq.js

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,5 +1,11 @@
 document.querySelectorAll('.question-card > h4').forEach((header) => {
     header.addEventListener('click', () => {
+        document.querySelectorAll('.question-card').forEach((card) => {
+            if (card !== header.parentElement) {
+                card.classList.remove('active')
+            }
+        })
+
         header.parentElement.classList.toggle('active')
     })
 })
