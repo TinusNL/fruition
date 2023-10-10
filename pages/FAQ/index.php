250a9eed7b270bb358bcc4fcadad060e14fd5c41
alvin-dev32 <alvinogbeifun6@gmail.com>
2023-10-10 14:44:48 +0100

    index fix

--- pages/FAQ/index.php
+++ pages/FAQ/index.php
@@ -1,16 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8" />
-    <title>FAQ Page</title>
-</head>
-<body>
+<div class="faq-div"></div>
+  <div class="faq_content" style=background-color:#9BCE77;>
     <h1>FAQ</h1>
-    <h3>frequently asked questions:</h3>
-    <h5>How do I secure my account?</h5>
-    <h5>how to add a new location?</h5>
-    <h5>how to upload images</h5>
-    <h5>how to change email</h5>
-    <h3>Need further assistance? feel free to chat with our chatbot Fruity!</h3>
-</body>
-</html>
+    <div class="search">
+      <input type="text" placeholder="Search...">
+    </div>
+    <div class="faq_questions">
+      <h3>frequently asked questions:</h3>
+      <h5>How do I secure my account?</h5>
+      <h5>how to add a new location?</h5>
+      <h5>how to upload images</h5>
+      <h5>how to change email</h5>
+
+    </div>
+
+
+    <h3>Need further assistance? feel free to chat with ourchatbot Fuity!</h3>
+
+
+    <div class="back_button">
+      <button class="b-button" type="button">Back</button>
+    </div>
+
+  </div>
+  <div class="faq-div"></div>
