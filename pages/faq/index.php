525086648b0edff759f187c96e869f215189a68a
Chloe7725 <94675582+Chloe7725@users.noreply.github.com>
2023-10-12 15:22:26 +0100

    faq answers

--- pages/faq/index.php
+++ pages/faq/index.php
@@ -9,23 +9,33 @@
         <h3>Frequently asked questions:</h3>
         <details class="details">
             <summary>How do I secure my account?</summary>
-            <p>First create a password. Make sure it is a unique password.</p>
+            <p>We have a secure password system that protects all accounts within our network.
+                We believe that th user can pick there own passwords however we suggest that you use these requirements
+                in order to make your account as secure as possible.
+            </p>
         </details>
         <details>
             <summary>How to manage account?</summary>
-            <p>Here you can find the answer of this question.</p>
+            <p> Go into settings and the option for account should appear, click on the then you’ll be taken to the alter account page.
+            </p>
         </details>
         <details>
             <summary>How to add a new location?</summary>
-            <p>Here you can find the answer of this question.</p>
+            <p>
+                By clicking the box with and up facing arrow you will be taken to the submit
+                a location page where you will then be able to submit a location.
+            </p>
         </details>
         <details>
             <summary>How to upload images?</summary>
-            <p>Here you can find the answer of this question.</p>
+            <p>Uploading images of a location can be done by going to the submit a location page and selecting upload.</p>
         </details>
         <details>
             <summary>How to change email?</summary>
-            <p>Here you can find the answer of this question.</p>
+            <p> 
+                By going to the settings icon and clicking it the clicking account you will 
+                be taken to the alter account page where you will be able to change your email.
+            </p>
         </details>
     </div>
     <h3 id="text">
