5739afcbedbc08fa3f7709058b751e57a310a4c4
Misha Opstal <6018369@mborijnland.nl>
2023-10-13 23:45:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,13 +22,13 @@ if (Router::isApi()) {
         <meta name="description" content="Fruition">
         <meta name="keywords" content="Fruition">
         <meta name="author" content="Plymouth City College & MBORijnland exchange project team">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="apple-touch-icon" sizes="180x180" href="/<?= URL_PREFIX ?>/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/<?= URL_PREFIX ?>/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/<?= URL_PREFIX ?>/favicon-16x16.png">
+        <link rel="manifest" href="/<?= URL_PREFIX ?>/site.webmanifest">
+        <link rel="mask-icon" href="/<?= URL_PREFIX ?>/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#2b5797">
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+        <meta name="msapplication-TileImage" content="/<?= URL_PREFIX ?>/mstile-144x144.png">
         <meta name="theme-color" content="#ffffff">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./<?= Router::getOffset() ?>css/main.css">
