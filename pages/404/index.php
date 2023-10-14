fb724e4a1a49d77c8602bf08aa57bc439b1d990e
Thanospoop1 <1387679@cityplym.ac.uk>
2023-10-14 19:55:03 +0100

    some fixed
    fixed question mark colour
    fixed 404 not taking you back

--- pages/404/index.php
+++ pages/404/index.php
@@ -2,7 +2,7 @@
     <div class="fourofour_left">
         <p class="fourofour">404</p>
         <p class="sorry_we_c"> Sorry we couldn’t find what you were looking for </p>
-        <a class="go_back" href="">Go back</a>
+        <a class="go_back" onclick="history.back()">Go back</a>
     </div>
     <div class="logo_404">
         <img src="./assets/logo.svg" alt="logo">
